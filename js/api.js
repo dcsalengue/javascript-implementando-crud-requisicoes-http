ad364d2d08f8a52addc33c97106d0ff8760cf996
dcsalengue <dcsalengue@me.com>
2025-01-19 22:14:02 -0300

    Aula 1 npm i -g json-server npm init -y Iniciando json server simulando o backend Acessando o banco de dados do json através do dom ao carregar a página

---
+++ js/api.js
@@ -0,0 +1,12 @@
+const api = {
+    async buscarPensamentos() {
+        try {
+            const response = await fetch('http://localhost:3000/pensamentos');
+            return await response.json();
+        } catch (error) {
+            alert(`Erro ao buscar pensamentos \r\n${error}`);
+            throw error;
+        }
+    }
+}
+export default api;
